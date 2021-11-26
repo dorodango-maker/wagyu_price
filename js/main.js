cb5445ea5b4f6d49bc2212f229fd3b59604321c1
つちだなおき <>
2021-11-26 18:11:06 +0900

    スムーススクロールをボタンを押した時だけに変更しました

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 $(function() {
-  $('a[href^="#"]').click(function() {
+  $('#page-top').children('a[href^="#"]').click(function() {
     var href = $(this).attr('href');
     var target = $(href == '#' || href == '' ? 'html' : href);
     var position = target.offset().top;
